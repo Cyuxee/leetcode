34a81e445352195eb6b5f9501a99424a6ee24c02
Cyuxee <85378478+Cyuxee@users.noreply.github.com>
2026-07-26 20:23:21 +0800

    628 Updated.

--- 1-1000/628. Maximum Product of Three Numbers (Daily).cpp
+++ 1-1000/628. Maximum Product of Three Numbers (Daily).cpp
@@ -1,3 +1,30 @@
+// Given an integer array nums, find three numbers whose product is maximum and return the maximum product.
+
+
+// Example 1:
+
+// Input: nums = [1,2,3]
+// Output: 6
+// Example 2:
+
+// Input: nums = [1,2,3,4]
+// Output: 24
+// Example 3:
+
+// Input: nums = [-1,-2,-3]
+// Output: -6
+ 
+
+// Constraints:
+
+// 3 <= nums.length <= 104
+// -1000 <= nums[i] <= 1000
+
+
+// 這題暴力解 O(N^3) 過不了。
+// 所以思路是先拆成正負數各一邊，並對各自做排序後，
+// 再考慮正負數要各拿幾個才會使得三個數乘積最大。
+
 class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
